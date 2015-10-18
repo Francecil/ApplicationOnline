09a1d90241109175378369727a1e1be5706206cc
Francecil <396324491@qq.com>
2015-10-18 17:09:55 +0800

    Test3

--- src/com/france/bean/User.java
+++ src/com/france/bean/User.java
@@ -1,5 +1,27 @@
 package com.france.bean;
 
 public class User {
+private int uid;
+private String uname;
+private String email;
+private String password;
+public String getUname() {
+	return uname;
+}
+public void setUname(String uname) {
+	this.uname = uname;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
 
 }
